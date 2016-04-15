2a0a2bee763565fd1f1a97f1fa17acbd5d2b8040
Kanmi Durotoye <admin@test.firmbase.net>
2016-04-14 21:22:59 -0400

    Import openjdk version 0.1.2

---
+++ cookbooks/openjdk/attributes/default.rb
@@ -0,0 +1,1 @@
+default['openjdk']['version'] = 7
---
+++ cookbooks/openjdk/metadata.rb
@@ -0,0 +1,16 @@
+name 'openjdk'
+maintainer 'Blake Irvin'
+maintainer_email 'blake.irvin@gmail.com'
+license 'mit'
+description 'Installs/Configures openjdk'
+long_description 'Installs/Configures openjdk'
+version '0.1.2'
+
+depends 'apt'
+
+%w{smartos ubuntu}.each do |os|
+  supports os
+end
+
+source_url       'https://github.com/bixu/openjdk-cookbook'
+issues_url       'https://github.com/bixu/openjdk-cookbook/issues'
